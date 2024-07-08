47767a196c453b10dd9a2ab56bd38691310c7e2e
YKDman <dbsrudejr300@gmail.com>
2024-07-08 23:43:47 +0900

    notemodal : note content, note footerbutton 작업전

--- src/feature/notes/ui/NoteInfoModal/NoteInfoModal.css.ts
+++ src/feature/notes/ui/NoteInfoModal/NoteInfoModal.css.ts
@@ -18,15 +18,45 @@ export const modalBackDrop = style({
   zIndex: -10,
   width: "100%",
   height: "100%",
-  backgroundColor: "rgba(0,0,0,0.2)",
+  backgroundColor: "rgba(0,0,0,0.6)",
   position: "relative",
 });
 
 export const noteModalContent = style({
-  width: "500px",
-  height: "500px",
+  width: "720px",
+  height: "600px",
   backgroundColor: "#fff",
   position: "absolute",
+  borderRadius: 15,
+  padding: vars.space.big2,
+  boxShadow: vars.shadow.basic,
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "stretch",
+});
+
+export const closeButtonArea = style({
+  position: "absolute",
+  top: 0,
+  right: vars.space.small,
+  cursor: "pointer",
+  width: vars.sizeRem.big2,
+  height: vars.sizeRem.big2,
+});
+
+export const closeButton = style({
+  width: "100%",
+  height: "100%",
+  border: "none",
+  backgroundColor: "transparent",
+  cursor: "pointer",
+  ":hover": {
+    opacity: 0.8,
+  },
+  ":active": {
+    opacity: 0.9,
+  },
+  fontSize: vars.fontSize.T2,
 });
 
 export const noteModalHeader = style({
@@ -35,4 +65,24 @@ export const noteModalHeader = style({
   textAlign: "center",
   paddingTop: vars.space.medium,
   position: "relative",
+  marginBottom: vars.space.big2,
+  fontSize: vars.fontSize.T2,
 });
+
+export const noteModalTitleArea = style({
+  width: "100%",
+  display: "flex",
+  alignItems: "center",
+});
+
+export const noteModalTitleLabel = style({
+  marginRight: vars.space.big2,
+});
+
+export const noteModalTitle = style({});
+
+export const noteContentArea = style({});
+
+export const footerArea = style({});
+
+export const footerButtonArea = style({});
--- src/feature/notes/ui/NoteInfoModal/NoteInfoModal.tsx
+++ src/feature/notes/ui/NoteInfoModal/NoteInfoModal.tsx
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { act, ChangeEvent, useState } from "react";
 import { useNoteStore } from "../../../../store/noteStore";
 import useModalStore from "../../../../store/modalStore";
 import {
+  closeButton,
+  closeButtonArea,
+  footerArea,
+  footerButtonArea,
   modalBackDrop,
   modalWrapper,
+  noteContentArea,
   noteModalContent,
   noteModalHeader,
+  noteModalTitle,
+  noteModalTitleArea,
+  noteModalTitleLabel,
 } from "./NoteInfoModal.css";
 import { IoMdClose } from "react-icons/io";
 
@@ -17,21 +25,44 @@ const NoteInfoModal: React.FC = () => {
     modalClose();
   };
 
+  const [currentNoteTitle, setCurrentNoteTitle] = useState<string>(
+    activeNote.title
+  );
+
+  const handleTitleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setCurrentNoteTitle(e.target.value);
+  };
+
   return modalOpen ? (
     <div className={modalWrapper}>
       <div className={modalBackDrop} onClick={handleBackDropClick}></div>
       <div className={noteModalContent}>
         <div className={noteModalHeader}>
           <h4>My Note</h4>
-          <IoMdClose />
+          <div className={closeButtonArea}>
+            <button className={closeButton} onClick={modalClose}>
+              <IoMdClose />
+            </button>
+          </div>
         </div>
-        <div>
-          <label htmlFor="">Note 제목</label>
-          <input type="text" value={activeNote.title} />
+        <div className={noteModalTitleArea}>
+          <label htmlFor="note-title" className={noteModalTitleLabel}>
+            제목
+          </label>
+          <input
+            type="text"
+            value={currentNoteTitle}
+            id="note-title"
+            className={noteModalTitle}
+            onChange={handleTitleChange}
+          />
         </div>
-        <div>modal content</div>
-        <div>
-          <button>x</button>
+        <div className={noteContentArea}>modal content</div>
+        <div className={footerArea}>
+          <div className={footerButtonArea}>
+            <button>저장</button>
+            <button onClick={modalClose}>닫기</button>
+          </div>
         </div>
       </div>
     </div>
